fec8cbc004319d985ca5845c028e5e39c913b561
abisoye <beesoye111@gmail.com>
2022-04-27 10:13:13 +0100

    use of jquery to collect data

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#addressbook").submit(function (event) {
+        event.preventDefault();
+
+
+        const  name1 = $("$name1").val();
+        const  name2 = $("$name2").val();
+        const name3 = $("$name3").val();
+        const comp = $("$comp").val();
+        const mail = $("$mail").val();
+        const $number = $("$number").val();
+        const fax = $("$fax").val();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+});
