3fe20540c8bbad2a4f6096d98bd8aceb1a789862
abisoye <beesoye111@gmail.com>
2022-04-27 11:24:38 +0100

    use of forms

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,28 @@ $(document).ready(function () {
         event.preventDefault();
 
 
-        const  name1 = $("$name1").val();
-        const  name2 = $("$name2").val();
-        const name3 = $("$name3").val();
-        const comp = $("$comp").val();
-        const mail = $("$mail").val();
-        const $number = $("$number").val();
-        const fax = $("$fax").val();
+        const name1 = $("#name1").val();
+        const name2 = $("#name2").val();
+        const name3 = $("#name3").val();
+        const comp = $("#comp").val();
+        const mail = $("#mail").val();
+        const number = $("#number").val();
+        const fax = $("#fax").val();
 
 
+        $(".firstName").append(name1);
+        $(".middleName").append(name2);
+        $(".lastName").append(name3);
+        $(".company").append(comp);
+        $(".email").append(mail);
+        $(".mobileNumber").append(number);
+        $(".faxNumber").append(fax);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        $(".firstName").click(function() {
+            $(".contact-details").show();
+        })
+        
+        $(".form").hide()
 
     });
 });
